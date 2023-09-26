7ed9b7fe81848153531a46bf3d5eb3e94a940432
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-09-27 01:32:10 +0200

    Basic completed

--- src/App.jsx
+++ src/App.jsx
@@ -16,41 +16,45 @@ function App() {
 
   return (
     <div>
-      <input
-        type="text"
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
-      {result.map((result, index) => (
-        <div key={index}>
-          <h2>Word: </h2>
-          <p>{result.word}</p>
-          <h2>Phonetic: </h2>
-          <p> {result.phonetic}</p>
+      <div className="search-container">
+        <input
+          type="text"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="cards-container">
+        {result.map((result, index) => (
+          <div key={index} className="card">
+            <h2>Word: </h2>
+            <p>{result.word}</p>
+            <h2>Phonetic: </h2>
+            <p> {result.phonetic}</p>
 
-          {result.phonetics.map((phonetic, index) => (
-            <div key={index}>
-              <h2>Phonetic Text: </h2>
-              <p>{phonetic.text}</p>
-              <audio controls src={phonetic.audio}></audio>
-            </div>
-          ))}
-          {result.meanings.map((meaning, index) => (
-            <div key={index}>
-              <h2>Part Of Speech: </h2> <p>{meaning.partOfSpeech}</p>
-              {meaning.definitions.map((definition, index) => (
-                <div key={index}>
-                  <h2>Definition: </h2>
-                  <p> {definition.definition}</p>
-                  <h2>Example: </h2>
-                  <p>{definition.example} </p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      ))}
+            {result.phonetics.map((phonetic, index) => (
+              <div key={index}>
+                <h2>Phonetic Text: </h2>
+                <p>{phonetic.text}</p>
+                <audio controls src={phonetic.audio}></audio>
+              </div>
+            ))}
+            {result.meanings.map((meaning, index) => (
+              <div key={index}>
+                <h2>Part Of Speech: </h2> <p>{meaning.partOfSpeech}</p>
+                {meaning.definitions.map((definition, index) => (
+                  <div key={index}>
+                    <h2>Definition: </h2>
+                    <p> {definition.definition}</p>
+                    <h2>Example: </h2>
+                    <p>{definition.example} </p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
